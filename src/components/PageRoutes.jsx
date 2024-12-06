bcdc5dcd33ca03390f1bac41c18a1116d28611f3
Md Shahanwaz <md.shahanwaz@rungta.org>
2024-12-06 16:28:07 +0530

    Update PageRoutes.jsx

--- src/components/PageRoutes.jsx
+++ src/components/PageRoutes.jsx
@@ -1,18 +1,24 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
-import { Logout } from "../pages/Logout"
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { Logout } from "../pages/Logout";
+import About from "../pages/About"; // Import the About page
+import Services from "../pages/Services"; // Import the Services page
+import Hospital from "../pages/Hospital"; // Import the Hospital component (adjust the path if needed)
 
 export const PageRoutes = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/logout" element={<Logout />} />
-            </Routes>
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/about" element={<About />} /> {/* About route */}
+        <Route path="/services" element={<Services />} /> {/* Services route */}
+        <Route path="/hospital" element={<Hospital />} /> {/* Hospital route */}
+      </Routes>
+    </>
+  );
+};
